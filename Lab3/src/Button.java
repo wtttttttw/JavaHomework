2b522467deb0947172bec0a418c1308d27f92210
wtttttttw <nastya.chudova01@gmail.com>
2025-09-26 20:01:44 +0700

    Delete Lab3 directory

--- Lab3/src/Button.java
+++
@@ -1,9 +0,0 @@
-public class Button {
-    private int clks = 0;
-    public void click(){
-        clks++;
-        System.out.println("Your button was clicked " + clks + " times");
-    }; 
-}
-
-
